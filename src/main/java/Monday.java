2819f04f621d6c52a4c3442395d42d57f456692b
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-08-21 16:21:19 +0800

    Completed Level-0: Rename, Greet, Exit

---
+++ src/main/java/Monday.java
@@ -0,0 +1,15 @@
+public class Monday {
+    public static void main(String[] args) {
+        String logo = " __  __                 _             \n"
+                + "|  \\/  |               | |            \n"
+                + "| \\  / | ___  _ __   __| | __ _ _   _  \n"
+                + "| |\\/| |/ _ \\| '_ \\ / _` |/ _` | | | | \n"
+                + "| |  | | (_) | | | | (_| | (_| | |_| | \n"
+                + "|_|  |_|\\___/|_| |_|\\__,_|\\__,_|\\__, | \n"
+                + "                                __/ | \n"
+                + "                               |___/  \n";
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+}
