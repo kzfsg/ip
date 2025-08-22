67d5febcb46079151174226eb0f559d0d196d925
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-08-22 11:13:56 +0800

    Level-3: Mark as Done

--- src/main/java/Monday.java
+++ src/main/java/Monday.java
@@ -24,7 +24,7 @@ public class Monday {
     private static void runChatbot() {
         Scanner scanner = new Scanner(System.in);
         String input;
-        ArrayList<String> inputs = new ArrayList<>();
+        ArrayList<Task> inputs = new ArrayList<>();
 
         while (true) {
             input = scanner.nextLine();
@@ -33,7 +33,24 @@ public class Monday {
                 break;
             } else if (input.equals("list")) {
                 displayList(inputs);
-            } else {
+            } else if (input.startsWith("mark ")) {
+                try {
+                    int taskNum = Integer.parseInt(input.substring(5));
+                    markTask(inputs, taskNum - 1);
+                    System.out.println("Nice! I've marked this task as done:\n  " + inputs.get(taskNum - 1));
+                } catch (Exception e) {
+                    System.out.println("Invalid task number.");
+                }
+            } else if (input.startsWith("unmark ")) {
+                try {
+                    int taskNum = Integer.parseInt(input.substring(7));
+                    unmarkTask(inputs, taskNum - 1);
+                    System.out.println("OK, I've marked this task as not done yet:\n  " + inputs.get(taskNum - 1));
+                } catch (Exception e) {
+                    System.out.println("Invalid task number.");
+                }
+            }
+            else {
                 addTask(inputs, input);
             }
         }
@@ -41,14 +58,24 @@ public class Monday {
         scanner.close();
     }
 
-    private static void displayList(ArrayList<String> inputs) {
+    private static void displayList(ArrayList<Task> inputs) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < inputs.size(); i++) {
-            System.out.println(i + ". " + inputs.get(i));
+            System.out.println((i + 1) + "." + inputs.get(i));
         }
     }
 
-    private static void addTask(ArrayList<String> inputs, String input) {
+    private static void markTask(ArrayList<Task> inputs, int index) {
+        inputs.get(index).markAsDone();
+    }
+
+    private static void unmarkTask(ArrayList<Task> inputs, int index) {
+        inputs.get(index).markAsNotDone();
+    }
+
+    private static void addTask(ArrayList<Task> inputs, String input) {
         System.out.println("added: " + input);
-        inputs.add(input);
+        inputs.add(new Task(input));
     }
 }
+
