2aa645fdf5f743d317ee448c18bd2edcb59d02da
kzfsg <131367782+kzfsg@users.noreply.github.com>
2025-08-29 14:46:21 +0800

    Add javaDoc comments to preliminary files like TaskList and Todo

--- src/main/java/monday/task/Task.java
+++ src/main/java/monday/task/Task.java
@@ -1,38 +1,83 @@
 package monday.task;
 
+/**
+ * Represents a generic task with a description and completion status.
+ * This is the base class for all task types in the Monday task manager.
+ * Subclasses can extend this to provide specialized behavior for different task types.
+ */
 public class Task {
-    // Changed to protected to allow subclass access while maintaining encapsulation
-    // This enables subclasses to access core data without breaking the abstraction
+    /**
+     * The description of the task.
+     * Protected to allow subclass access while maintaining encapsulation.
+     */
     protected String description;
+    
+    /**
+     * The completion status of the task.
+     * True if the task has been completed, false otherwise.
+     */
     protected boolean isDone;
 
+    /**
+     * Constructs a new Task with the specified description.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the description of this task.
+     *
+     * @return The task description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Checks if this task has been completed.
+     *
+     * @return true if the task is done, false otherwise
+     */
     public boolean isDone() {
         return isDone;
     }
 
+    /**
+     * Marks this task as completed.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks this task as not completed.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon for this task.
+     * Shows [X] for completed tasks and [ ] for incomplete tasks.
+     *
+     * @return The status icon string
+     */
     public String getStatusIcon() {
         return isDone ? "[X]" : "[ ]";
     }
 
-    // Virtual method that subclasses can override for specialized display
-    // Base implementation works for simple todos
+    /**
+     * Returns a string representation of this task.
+     * The format is "[status icon] [description]".
+     * Subclasses can override this method to provide specialized formatting.
+     *
+     * @return A string representation of this task
+     */
     @Override
     public String toString() {
         return getStatusIcon() + " " + description;
--- src/main/java/monday/task/Todo.java
+++ src/main/java/monday/task/Todo.java
@@ -1,10 +1,31 @@
 package monday.task;
 
+/**
+ * Represents a simple todo task without any date/time constraints.
+ * This is the most basic type of task in the Monday task manager.
+ * Todo tasks are displayed with a [T] prefix to distinguish them from other task types.
+ */
 public class Todo extends Task {
+    
+    /**
+     * Constructs a new Todo task with the specified description.
+     * The todo is initially marked as not done.
+     *
+     * @param description The description of the todo task
+     */
     public Todo(String description) {
         super(description);
     }
 
+    /**
+     * Returns a string representation of this todo task.
+     * The format is "[T][status icon] [description]" where:
+     * - [T] indicates this is a Todo task
+     * - [status icon] shows [X] if done, [ ] if not done
+     * - [description] is the task description
+     *
+     * @return A formatted string representation of this todo task
+     */
     @Override
     public String toString() {
         return "[T]" + getStatusIcon() + " " + description;
